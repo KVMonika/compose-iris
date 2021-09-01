54adcb7db8972dd0c03e9b91527382d6f472ca5f
KVMonika <korrapatimonika@gmail.com>
2021-09-01 19:40:59 +0530

    Dataset specific changes

--- predictr/main.py
+++ predictr/main.py
@@ -8,15 +8,41 @@ app = FastAPI(title="predictr", docs_url="/")
 
 # class which is expected in the payload
 class QueryIn(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
+    mean_radius: float
+    mean_texture: float
+    mean_perimeter: float
+    mean_area: float
+    mean_smoothness: float
+    mean_compactness: float
+    mean_concavity: float
+    mean_concave_points: float
+    mean_symmetry: float
+    mean_fractal_dimension: float
+    radius_error: float
+    texture_error: float
+    perimeter_error: float
+    area_error: float
+    smoothness_error: float
+    compactness_error: float
+    concavity_error: float
+    concave_points_error: float
+    symmetry_error: float
+    fractal_dimension_error: float
+    worst_radius: float
+    worst_texture: float
+    worst_perimeter: float
+    worst_area: float
+    worst_smoothness: float
+    worst_compactness: float
+    worst_concavity: float
+    worst_concave_points: float
+    worst_symmetry: float
+    worst_fractal_dimension: float
 
 
 # class which is returned in the response
 class QueryOut(BaseModel):
-    flower_class: str
+    cancer_class: str
 
 
 # Route definitions
@@ -31,7 +57,7 @@ def ping():
 # Payload: QueryIn containing the parameters
 # Response: QueryOut containing the flower_class predicted (200)
 def predict_flower(query_data: QueryIn):
-    output = {"flower_class": predict(query_data)}
+    output = {"cancer_class": predict(query_data)}
     return output
 
 
--- trainr/main.py
+++ trainr/main.py
@@ -18,11 +18,37 @@ app.add_event_handler("startup", init_model)
 
 # class which is expected in the payload while training
 class TrainIn(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
-    flower_class: str
+    mean_radius: float
+    mean_texture: float
+    mean_perimeter: float
+    mean_area: float
+    mean_smoothness: float
+    mean_compactness: float
+    mean_concavity: float
+    mean_concave_points: float
+    mean_symmetry: float
+    mean_fractal_dimension: float
+    radius_error: float
+    texture_error: float
+    perimeter_error: float
+    area_error: float
+    smoothness_error: float
+    compactness_error: float
+    concavity_error: float
+    concave_points_error: float
+    symmetry_error: float
+    fractal_dimension_error: float
+    worst_radius: float
+    worst_texture: float
+    worst_perimeter: float
+    worst_area: float
+    worst_smoothness: float
+    worst_compactness: float
+    worst_concavity: float
+    worst_concave_points: float
+    worst_symmetry: float
+    worst_fractal_dimension: float
+    cancer_class: str
 
 
 # Route definitions
